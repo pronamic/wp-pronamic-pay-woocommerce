e18e1374bf17779e0cb0ec4df75645965d2f548b
Remco Tolsma <info@remcotolsma.nl>
2014-11-05 14:25:45 +0100

    Added credit card gateway.

--- src/Extension.php
+++ src/Extension.php
@@ -47,7 +47,7 @@ class Pronamic_WP_Pay_Extensions_WooCommerce_Extension {
 	 * Add the gateway to WooCommerce
 	 */
 	public static function payment_gateways( $gateways ) {
-		$gateways[] = 'Pronamic_WP_Pay_Extensions_WooCommerce_DirectDebitGateway';
+		$gateways[] = 'Pronamic_WP_Pay_Extensions_WooCommerce_CreditCardGateway';
 		$gateways[] = 'Pronamic_WP_Pay_Extensions_WooCommerce_IDealGateway';
 		$gateways[] = 'Pronamic_WP_Pay_Extensions_WooCommerce_MiniTixGateway';
 		$gateways[] = 'Pronamic_WP_Pay_Extensions_WooCommerce_MisterCashGateway';
