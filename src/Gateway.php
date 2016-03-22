9c65b0184ed585ed6eb569728eae1bc75f63894a
Remco Tolsma <info@remcotolsma.nl>
2016-03-22 12:09:10 +0100

    Also use the global default config as the WooCommerce default config.

--- src/Gateway.php
+++ src/Gateway.php
@@ -123,7 +123,7 @@ class Pronamic_WP_Pay_Extensions_WooCommerce_Gateway extends WC_Payment_Gateway 
 			'config_id'   => array(
 				'title'       => __( 'Configuration', 'pronamic_ideal' ),
 				'type'        => 'select',
-				'default'     => '',
+				'default'     => get_option( 'pronamic_pay_config_id' ),
 				'options'     => Pronamic_WP_Pay_Plugin::get_config_select_options( $this->payment_method ),
 			),
 			'payment' => array(
