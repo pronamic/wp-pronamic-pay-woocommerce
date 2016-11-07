130e693b1704f062f7b29e130da53c5529bbd08e
Remco Tolsma <info@remcotolsma.nl>
2016-11-07 14:23:20 +0100

    Added Maestro gateway.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,9 @@ This projects adheres to [Semantic Versioning](http://semver.org/) and [Keep a C
 ## [Unreleased][unreleased]
 -
 
+## [1.2.2] - Unreleased
+- Added Maestro gateway.
+
 ## [1.2.1] - 2016-10-20
 - Added experimental support for WooCommerce Subscriptions / recurring payments.
 - Restore compatibility with WooCommerce versions < 2.2.0.
@@ -66,7 +69,8 @@ This projects adheres to [Semantic Versioning](http://semver.org/) and [Keep a C
 ## 1.0.0 - 2014-12-19
 - First release.
 
-[unreleased]: https://github.com/wp-pay-extensions/woocommerce/compare/1.2.1...HEAD
+[unreleased]: https://github.com/wp-pay-extensions/woocommerce/compare/1.2.2...HEAD
+[1.2.2]: https://github.com/wp-pay-extensions/woocommerce/compare/1.2.1...1.2.2
 [1.2.1]: https://github.com/wp-pay-extensions/woocommerce/compare/1.2.0...1.2.1
 [1.2.0]: https://github.com/wp-pay-extensions/woocommerce/compare/1.1.9...1.2.0
 [1.1.9]: https://github.com/wp-pay-extensions/woocommerce/compare/1.1.8...1.1.9
---
+++ src/MaestroGateway.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Title: WooCommerce Maestro gateway
+ * Description:
+ * Copyright: Copyright (c) 2005 - 2016
+ * Company: Pronamic
+ *
+ * @author Remco Tolsma
+ * @version 1.2.2
+ * @since 1.2.2
+ */
+class Pronamic_WP_Pay_Extensions_WooCommerce_MaestroGateway extends Pronamic_WP_Pay_Extensions_WooCommerce_Gateway {
+	/**
+	 * The unique ID of this payment gateway
+	 *
+	 * @var string
+	 */
+	const ID = 'pronamic_pay_maestro';
+
+	//////////////////////////////////////////////////
+
+	/**
+	 * Constructs and initialize an iDEAL gateway
+	 */
+	public function __construct() {
+		$this->id             = self::ID;
+		$this->method_title   = __( 'Maestro', 'pronamic_ideal' );
+		$this->payment_method = Pronamic_WP_Pay_PaymentMethods::MAESTRO;
+
+		parent::__construct();
+	}
+
+	/**
+	 * Initialise form fields
+	 */
+	function init_form_fields() {
+		parent::init_form_fields();
+
+		$this->form_fields['enabled']['label']       = __( 'Enable Maestro', 'pronamic_ideal' );
+		$this->form_fields['description']['default'] = '';
+	}
+}
