b8557336f8eea65b4b068ba1ebf308d1281ec09b
Reüel van der Steege <reuel@pronamic.nl>
2017-03-03 16:20:31 +0100

    Don't set subscriptions 'on hold' due to delay in direct debit status update.

--- src/Gateway.php
+++ src/Gateway.php
@@ -268,8 +268,6 @@ class Pronamic_WP_Pay_Extensions_WooCommerce_Gateway extends WC_Payment_Gateway 
 		}
 
 		foreach ( $subscriptions as $subscription_id => $subscription ) {
-			$subscription->update_status( 'on-hold', __( 'Subscription renewal payment due.', 'pronamic_ideal' ) );
-
 			if ( ! $subscription->is_manual() ) {
 				$order->set_payment_method( $subscription->payment_gateway );
 
