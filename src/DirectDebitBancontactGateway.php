pronamic/wp-pronamic-pay-woocommerce

commit f1d42d727aa55b487b67bd1f756c32d98094bae0

DRY.

src/DirectDebitBancontactGateway.php
@@ -18,23 +18,9 @@ use WC_Subscriptions_Cart;
  */
 class DirectDebitBancontactGateway extends Gateway {
 	/**
-	 * The unique ID of this payment gateway
-	 *
-	 * @var string
-	 */
-	const ID = 'pronamic_pay_direct_debit_bancontact';
-
-	/**
-	 * Payment method.
-	 *
-	 * @var string
-	 */
-	protected $payment_method = PaymentMethods::DIRECT_DEBIT_BANCONTACT;
-
-	/**
 	 * Constructs and initialize an Direct Debit (mandate via Bancontact) gateway
 	 */
-	public function __construct() {
+	public function __construct( $args = array() ) {
 		// @since unreleased
 		$this->supports = array(
 			'products',
@@ -53,34 +39,7 @@ class DirectDebitBancontactGateway extends Gateway {
 		// Filters.
 		add_filter( 'woocommerce_available_payment_gateways', array( $this, 'get_available_payment_gateways' ) );
 
-		parent::__construct();
-	}
-
-	/**
-	 * Initialise form fields
-	 */
-	public function init_form_fields() {
-		parent::init_form_fields();
-
-		$description_prefix = '';
-
-		if ( WooCommerce::version_compare( '2.0.0', '<' ) ) {
-			$description_prefix = '<br />';
-		}
-
-		$this->form_fields['description']['default'] = sprintf(
-			/* translators: %s: payment method */
-			__( 'By using this payment method you authorize us via %s to debit payments from your bank account.', 'pronamic_ideal' ),
-			__( 'Bancontact', 'pronamic_ideal' )
-		);
-		$this->form_fields['icon']['default']     = plugins_url( 'images/sepa-bancontact/wc-sepa-bancontact.png', Plugin::$file );
-		$this->form_fields['icon']['description'] = sprintf(
-			'%s%s<br />%s',
-			$description_prefix,
-			__( 'This controls the icon which the user sees during checkout.', 'pronamic_ideal' ),
-			/* translators: %s: default icon URL */
-			sprintf( __( 'Default: <code>%s</code>.', 'pronamic_ideal' ), $this->form_fields['icon']['default'] )
-		);
+		parent::__construct( $args );
 	}
 
 	/**

src/DirectDebitIDealGateway.php
@@ -18,24 +18,10 @@ use WC_Subscriptions_Cart;
  */
 class DirectDebitIDealGateway extends Gateway {
 	/**
-	 * The unique ID of this payment gateway
-	 *
-	 * @var string
-	 */
-	const ID = 'pronamic_pay_direct_debit_ideal';
-
-	/**
-	 * Payment method.
-	 *
-	 * @var string
-	 */
-	protected $payment_method = PaymentMethods::DIRECT_DEBIT_IDEAL;
-
-	/**
 	 * Constructs and initialize an Direct Debit (mandate via iDEAL) gateway
 	 */
-	public function __construct() {
-		parent::__construct();
+	public function __construct( $args = array() ) {
+		parent::__construct( $args );
 
 		// @since unreleased
 		$this->supports = array(
@@ -54,33 +40,6 @@ class DirectDebitIDealGateway extends Gateway {
 
 		// Filters.
 		add_filter( 'woocommerce_available_payment_gateways', array( $this, 'get_available_payment_gateways' ) );
-	}
-
-	/**
-	 * Initialise form fields
-	 */
-	public function init_form_fields() {
-		parent::init_form_fields();
-
-		$description_prefix = '';
-
-		if ( WooCommerce::version_compare( '2.0.0', '<' ) ) {
-			$description_prefix = '<br />';
-		}
-
-		$this->form_fields['description']['default'] = sprintf(
-			/* translators: %s: payment method */
-			__( 'By using this payment method you authorize us via %s to debit payments from your bank account.', 'pronamic_ideal' ),
-			__( 'iDEAL', 'pronamic_ideal' )
-		);
-		$this->form_fields['icon']['default']     = plugins_url( 'images/sepa-ideal/wc-sepa-ideal.png', Plugin::$file );
-		$this->form_fields['icon']['description'] = sprintf(
-			'%s%s<br />%s',
-			$description_prefix,
-			__( 'This controls the icon which the user sees during checkout.', 'pronamic_ideal' ),
-			/* translators: %s: default icon URL */
-			sprintf( __( 'Default: <code>%s</code>.', 'pronamic_ideal' ), $this->form_fields['icon']['default'] )
-		);
 	}
 
 	/**

src/DirectDebitSofortGateway.php
@@ -18,24 +18,10 @@ use WC_Subscriptions_Cart;
  */
 class DirectDebitSofortGateway extends Gateway {
 	/**
-	 * The unique ID of this payment gateway
-	 *
-	 * @var string
-	 */
-	const ID = 'pronamic_pay_direct_debit_sofort';
-
-	/**
-	 * Payment method.
-	 *
-	 * @var string
-	 */
-	protected $payment_method = PaymentMethods::DIRECT_DEBIT_SOFORT;
-
-	/**
 	 * Constructs and initialize an Direct Debit (mandate via Sofort) gateway
 	 */
-	public function __construct() {
-		parent::__construct();
+	public function __construct( $args = array() ) {
+		parent::__construct( $args );
 
 		// @since unreleased
 		$this->supports = array(
@@ -54,33 +40,6 @@ class DirectDebitSofortGateway extends Gateway {
 
 		// Filters.
 		add_filter( 'woocommerce_available_payment_gateways', array( $this, 'get_available_payment_gateways' ) );
-	}
-
-	/**
-	 * Initialise form fields
-	 */
-	public function init_form_fields() {
-		parent::init_form_fields();
-
-		$description_prefix = '';
-
-		if ( WooCommerce::version_compare( '2.0.0', '<' ) ) {
-			$description_prefix = '<br />';
-		}
-
-		$this->form_fields['description']['default'] = sprintf(
-			/* translators: %s: payment method */
-			__( 'By using this payment method you authorize us via %s to debit payments from your bank account.', 'pronamic_ideal' ),
-			__( 'SOFORT', 'pronamic_ideal' )
-		);
-		$this->form_fields['icon']['default']     = plugins_url( 'images/sepa-sofort/wc-sepa-sofort.png', Plugin::$file );
-		$this->form_fields['icon']['description'] = sprintf(
-			'%s%s<br />%s',
-			$description_prefix,
-			__( 'This controls the icon which the user sees during checkout.', 'pronamic_ideal' ),
-			/* translators: %s: default icon URL */
-			sprintf( __( 'Default: <code>%s</code>.', 'pronamic_ideal' ), $this->form_fields['icon']['default'] )
-		);
 	}
 
 	/**

src/AfterPayGateway.php
@@ -1,31 +1,0 @@
-<?php
-
-namespace Pronamic\WordPress\Pay\Extensions\WooCommerce;
-
-use Pronamic\WordPress\Pay\Core\PaymentMethods;
-
-/**
- * Title: WooCommerce AfterPay gateway.
- * Description:
- * Copyright: Copyright (c) 2005 - 2018
- * Company: Pronamic
- *
- * @author  Reüel van der Steege
- * @version 2.0.1
- * @since   2.0.1
- */
-class AfterPayGateway extends Gateway {
-	/**
-	 * The unique ID of this payment gateway
-	 *
-	 * @var string
-	 */
-	const ID = 'pronamic_pay_afterpay';
-
-	/**
-	 * Payment method.
-	 *
-	 * @var string
-	 */
-	protected $payment_method = PaymentMethods::AFTERPAY;
-}

src/AlipayGateway.php
@@ -1,31 +1,0 @@
-<?php
-
-namespace Pronamic\WordPress\Pay\Extensions\WooCommerce;
-
-use Pronamic\WordPress\Pay\Core\PaymentMethods;
-
-/**
- * Title: WooCommerce Alipay gateway
- * Description:
- * Copyright: Copyright (c) 2005 - 2018
- * Company: Pronamic
- *
- * @author  Reüel van der Steege
- * @version 2.0.0
- * @since   2.0.0
- */
-class AlipayGateway extends Gateway {
-	/**
-	 * The unique ID of this payment gateway
-	 *
-	 * @var string
-	 */
-	const ID = 'pronamic_pay_alipay';
-
-	/**
-	 * Payment method.
-	 *
-	 * @var string
-	 */
-	protected $payment_method = PaymentMethods::ALIPAY;
-}

src/BancontactGateway.php
@@ -1,54 +1,0 @@
-<?php
-
-namespace Pronamic\WordPress\Pay\Extensions\WooCommerce;
-
-use Pronamic\WordPress\Pay\Core\PaymentMethods;
-use Pronamic\WordPress\Pay\Plugin;
-
-/**
- * Title: WooCommerce Bancontact gateway
- * Description:
- * Copyright: Copyright (c) 2005 - 2018
- * Company: Pronamic
- *
- * @author  Remco Tolsma
- * @version 2.0.0
- * @since   1.0.0
- */
-class BancontactGateway extends Gateway {
-	/**
-	 * The unique ID of this payment gateway
-	 *
-	 * @var string
-	 */
-	const ID = 'pronamic_pay_mister_cash';
-
-	/**
-	 * Payment method.
-	 *
-	 * @var string
-	 */
-	protected $payment_method = PaymentMethods::BANCONTACT;
-
-	/**
-	 * Initialise form fields
-	 */
-	public function init_form_fields() {
-		parent::init_form_fields();
-
-		$description_prefix = '';
-
-		if ( WooCommerce::version_compare( '2.0.0', '<' ) ) {
-			$description_prefix = '<br />';
-		}
-
-		$this->form_fields['icon']['default']     = plugins_url( 'images/bancontact/wc-icon.png', Plugin::$file );
-		$this->form_fields['icon']['description'] = sprintf(
-			'%s%s<br />%s',
-			$description_prefix,
-			__( 'This controls the icon which the user sees during checkout.', 'pronamic_ideal' ),
-			/* translators: %s: default icon URL */
-			sprintf( __( 'Default: <code>%s</code>.', 'pronamic_ideal' ), $this->form_fields['icon']['default'] )
-		);
-	}
-}

src/BankTransferGateway.php
@@ -1,31 +1,0 @@
-<?php
-
-namespace Pronamic\WordPress\Pay\Extensions\WooCommerce;
-
-use Pronamic\WordPress\Pay\Core\PaymentMethods;
-
-/**
- * Title: WooCommerce Bank Transfer gateway
- * Description:
- * Copyright: Copyright (c) 2005 - 2018
- * Company: Pronamic
- *
- * @author  Remco Tolsma
- * @version 2.0.0
- * @since   1.0.0
- */
-class BankTransferGateway extends Gateway {
-	/**
-	 * The unique ID of this payment gateway
-	 *
-	 * @var string
-	 */
-	const ID = 'pronamic_pay_bank_transfer';
-
-	/**
-	 * Payment method.
-	 *
-	 * @var string
-	 */
-	protected $payment_method = PaymentMethods::BANK_TRANSFER;
-}

src/BelfiusGateway.php
@@ -1,31 +1,0 @@
-<?php
-
-namespace Pronamic\WordPress\Pay\Extensions\WooCommerce;
-
-use Pronamic\WordPress\Pay\Core\PaymentMethods;
-
-/**
- * Title: WooCommerce Belfius Direct Net gateway
- * Description:
- * Copyright: Copyright (c) 2005 - 2018
- * Company: Pronamic
- *
- * @author  Remco Tolsma
- * @version 2.0.0
- * @since   1.0.0
- */
-class BelfiusGateway extends Gateway {
-	/**
-	 * The unique ID of this payment gateway
-	 *
-	 * @var string
-	 */
-	const ID = 'pronamic_pay_belfius';
-
-	/**
-	 * Payment method.
-	 *
-	 * @var string
-	 */
-	protected $payment_method = PaymentMethods::BELFIUS;
-}

commit 83f7211ed1f9f22d61f45a3e63aac466d1ea48d0

Fix broken filtering of available gateways when no subscription product in cart (broken since f1d42d7).

src/DirectDebitBancontactGateway.php
@@ -2,8 +2,6 @@
 
 namespace Pronamic\WordPress\Pay\Extensions\WooCommerce;
 
-use Pronamic\WordPress\Pay\Core\PaymentMethods;
-use Pronamic\WordPress\Pay\Plugin;
 use WC_Subscriptions_Cart;
 
 /**
@@ -21,6 +19,8 @@ class DirectDebitBancontactGateway extends Gateway {
 	 * Constructs and initialize an Direct Debit (mandate via Bancontact) gateway
 	 */
 	public function __construct( $args = array() ) {
+		parent::__construct( $args );
+
 		// @since unreleased
 		$this->supports = array(
 			'products',
@@ -38,8 +38,6 @@ class DirectDebitBancontactGateway extends Gateway {
 
 		// Filters.
 		add_filter( 'woocommerce_available_payment_gateways', array( $this, 'get_available_payment_gateways' ) );
-
-		parent::__construct( $args );
 	}
 
 	/**
@@ -62,8 +60,8 @@ class DirectDebitBancontactGateway extends Gateway {
 			return $available_gateways;
 		}
 
-		if ( isset( $available_gateways[ self::ID ] ) ) {
-			unset( $available_gateways[ self::ID ] );
+		if ( isset( $available_gateways[ $this->id ] ) ) {
+			unset( $available_gateways[ $this->id ] );
 		}
 
 		return $available_gateways;

src/DirectDebitIDealGateway.php
@@ -2,8 +2,6 @@
 
 namespace Pronamic\WordPress\Pay\Extensions\WooCommerce;
 
-use Pronamic\WordPress\Pay\Core\PaymentMethods;
-use Pronamic\WordPress\Pay\Plugin;
 use WC_Subscriptions_Cart;
 
 /**
@@ -62,8 +60,8 @@ class DirectDebitIDealGateway extends Gateway {
 			return $available_gateways;
 		}
 
-		if ( isset( $available_gateways[ self::ID ] ) ) {
-			unset( $available_gateways[ self::ID ] );
+		if ( isset( $available_gateways[ $this->id ] ) ) {
+			unset( $available_gateways[ $this->id ] );
 		}
 
 		return $available_gateways;

src/DirectDebitSofortGateway.php
@@ -2,8 +2,6 @@
 
 namespace Pronamic\WordPress\Pay\Extensions\WooCommerce;
 
-use Pronamic\WordPress\Pay\Core\PaymentMethods;
-use Pronamic\WordPress\Pay\Plugin;
 use WC_Subscriptions_Cart;
 
 /**
@@ -62,8 +60,8 @@ class DirectDebitSofortGateway extends Gateway {
 			return $available_gateways;
 		}
 
-		if ( isset( $available_gateways[ self::ID ] ) ) {
-			unset( $available_gateways[ self::ID ] );
+		if ( isset( $available_gateways[ $this->id ] ) ) {
+			unset( $available_gateways[ $this->id ] );
 		}
 
 		return $available_gateways;