abe7d56b673a25e0d8eb7429d43a9fcf4f9201f6
Gautam Garg <admin@knitpay.org>
2021-02-02 18:49:21 +0530

    Some Payment Gateways need country name instead of country code. Update Gateway.php

--- src/Gateway.php
+++ src/Gateway.php
@@ -433,6 +433,7 @@ class Gateway extends WC_Payment_Gateway {
 
 		if ( ! empty( $billing_country ) ) {
 			$billing_address->set_country_code( $billing_country );
+			$billing_address->set_country_name( WC()->countries->countries[ $billing_country ] );
 		}
 
 		// Shipping address.
@@ -455,6 +456,7 @@ class Gateway extends WC_Payment_Gateway {
 
 		if ( ! empty( $shipping_country ) ) {
 			$shipping_address->set_country_code( $shipping_country );
+			$shipping_address->set_country_name( WC()->countries->countries[ $shipping_country ] );
 		}
 
 		// Issuer.
