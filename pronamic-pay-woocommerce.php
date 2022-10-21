70751b6c71f03f0a8617bc85590d451c4baec216
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2022-10-21 16:29:04 +0200

    Add plugin add-on file.

---
+++ pronamic-pay-woocommerce.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Plugin Name: Pronamic Pay WooCommerce Add-On
+ * Plugin URI: https://www.pronamic.eu/plugins/pronamic-pay-woocommerce/
+ * Description: Extend the Pronamic Pay plugin with WooCommerce support to receive payments through a variety of payment providers.
+ *
+ * Version: 4.2.0
+ * Requires at least: 4.7
+ *
+ * Author: Pronamic
+ * Author URI: https://www.pronamic.eu/
+ *
+ * Text Domain: pronamic-pay-woocommerce
+ * Domain Path: /languages/
+ *
+ * License: GPL-3.0-or-later
+ *
+ * Depends: wp-pay/core
+ *
+ * GitHub URI: https://github.com/pronamic/wp-pronamic-pay-woocommerce
+ *
+ * @author    Pronamic <info@pronamic.eu>
+ * @copyright 2005-2022 Pronamic
+ * @license   GPL-3.0-or-later
+ * @package   Pronamic\WordPress\Pay\Extensions\WooCommerce
+ */
+
+add_filter(
+	'pronamic_pay_plugin_integrations',
+	function ( $integrations ) {
+		foreach ( $integrations as $integration ) {
+			if ( $integration instanceof \Pronamic\WordPress\Pay\Extensions\WooCommerce\Extension ) {
+				return $integrations;
+			}
+		}
+
+		$integrations[] = new \Pronamic\WordPress\Pay\Extensions\WooCommerce\Extension();
+
+		return $integrations;
+	}
+);
