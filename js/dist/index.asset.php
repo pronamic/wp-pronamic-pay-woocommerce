63a44ae47a81d2d76a23cf373cd0b2b74bc5612a
Reüel van der Steege <reuel@pronamic.nl>
2022-10-27 16:44:42 +0200

    Fix blocks input fields not displayed after after merging #22.

--- js/dist/index.asset.php
+++ js/dist/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wc-blocks-registry', 'wc-settings', 'wp-components', 'wp-element', 'wp-html-entities'), 'version' => 'ed663a63742ad84039f4');
+<?php return array('dependencies' => array('wc-blocks-registry', 'wc-settings', 'wp-components', 'wp-element', 'wp-html-entities'), 'version' => '0008fbbd5c5af2ca9549');
