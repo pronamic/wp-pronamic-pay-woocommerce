01096bc1c386b129c892f9b0a2383f54089fbb2b
Reüel van der Steege <reuel@pronamic.nl>
2023-06-07 14:35:30 +0200

    Add subscription payment lines in subscription updater.

--- src/SubscriptionUpdater.php
+++ src/SubscriptionUpdater.php
@@ -11,7 +11,10 @@
 namespace Pronamic\WordPress\Pay\Extensions\WooCommerce;
 
 use Pronamic\WordPress\Money\Money;
+use Pronamic\WordPress\Money\TaxedMoney;
 use Pronamic\WordPress\Pay\Core\Util;
+use Pronamic\WordPress\Pay\Payments\PaymentLines;
+use Pronamic\WordPress\Pay\Payments\PaymentLineType;
 use Pronamic\WordPress\Pay\Subscriptions\Subscription;
 use Pronamic\WordPress\Pay\Subscriptions\SubscriptionInterval;
 use Pronamic\WordPress\Pay\Subscriptions\SubscriptionPhase;
@@ -92,6 +95,60 @@ class SubscriptionUpdater {
 				$woocommerce_subscription->get_id()
 			)
 		);
+
+		/*
+		 * Payment lines and order items.
+		 *
+		 * WooCommerce has multiple order item types:
+		 * `line_item`, `fee`, `shipping`, `tax`, `coupon`
+		 * @link https://github.com/woocommerce/woocommerce/search?q=%22extends+WC_Order_Item%22
+		 *
+		 * For now we handle only the `line_item`, `fee` and `shipping` items,
+		 * we consciously don't handle the `tax` and `coupon` items.
+		 *
+		 * **Order item `coupon`**
+		 * Coupon items are also applied to the `line_item` item and line total.
+		 * @link https://basecamp.com/1810084/projects/10966871/todos/372490988
+		 *
+		 * **Order item `tax`**
+		 * Tax items are also  applied to the `line_item` item and line total.
+		 */
+		$items = $woocommerce_subscription->get_items( [ 'line_item', 'fee', 'shipping' ] );
+
+		$tax_percentages = [ 0 ];
+
+		$pronamic_subscription->lines = new PaymentLines();
+
+		foreach ( $items as $item_id => $item ) {
+			$line = $pronamic_subscription->lines->new_line();
+
+			$type = OrderItemType::transform( $item );
+
+			// Quantity.
+			$quantity = \wc_stock_amount( $item['qty'] );
+
+			if ( PaymentLineType::SHIPPING === $type ) {
+				$quantity = 1;
+			}
+
+			// Tax.
+			$tax_rate_id = WooCommerce::get_order_item_tax_rate_id( $item );
+
+			$percent = is_null( $tax_rate_id ) ? null : \WC_Tax::get_rate_percent_value( $tax_rate_id );
+
+			// Set line properties.
+			$line->set_id( (string) $item_id );
+			$line->set_sku( WooCommerce::get_order_item_sku( $item ) );
+			$line->set_type( (string) $type );
+			$line->set_name( $item['name'] );
+			$line->set_quantity( $quantity );
+			$line->set_unit_price( new TaxedMoney( $woocommerce_subscription->get_item_total( $item, true ), WooCommerce::get_currency(), $woocommerce_subscription->get_item_tax( $item ), $percent ) );
+			$line->set_total_amount( new TaxedMoney( $woocommerce_subscription->get_line_total( $item, true ), WooCommerce::get_currency(), $woocommerce_subscription->get_line_tax( $item ), $percent ) );
+			$line->set_product_url( WooCommerce::get_order_item_url( $item ) );
+			$line->set_image_url( WooCommerce::get_order_item_image( $item ) );
+			$line->set_product_category( WooCommerce::get_order_item_category( $item ) );
+			$line->set_meta( 'woocommerce_order_item_id', $item_id );
+		}
 
 		// Phases.
 		$pronamic_subscription->set_phases( [] );
