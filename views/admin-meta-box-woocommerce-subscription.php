9e92e29fb5a552918268f2669534eaa94e312c4b
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2023-09-27 12:49:06 +0200

    Add Pronamic Pay subscription info the Pronamic Pay meta box.

--- views/admin-meta-box-woocommerce-subscription.php
+++ views/admin-meta-box-woocommerce-subscription.php
@@ -8,22 +8,70 @@
  * @package   Pronamic\WordPress\Pay
  */
 
-$payment_id = (int) $subscription->get_meta( '_pronamic_payment_id' );
+$pronamic_subscription_id = $subscription->get_meta( 'pronamic_subscription_id' );
 
-$payment = get_pronamic_payment( $payment_id );
+$pronamic_subscription = get_pronamic_subscription( $pronamic_subscription_id );
 
-?><style>
+$pronamic_payment_id = (int) $subscription->get_meta( '_pronamic_payment_id' );
+
+$pronamic_payment = get_pronamic_payment( $pronamic_payment_id );
+
+?>
+<style>
 	#woocommerce-subscription-pronamic-pay .inside {
 		margin: 0;
 		padding: 10px;
 	}
 
-	#woocommerce-subscription-pronamic-pay th {
+	#woocommerce-subscription-pronamic-pay h3 {
+		font-size: 14px;
+		margin: 0;
+	}
+
+	#woocommerce-subscription-pronamic-pay th,
+	#woocommerce-subscription-pronamic-pay td {
 		text-align: left;
+
+		width: 50%;
 	}
 </style>
 
-<?php if ( null === $payment ) : ?>
+<h3><?php esc_html_e( 'Subscription', 'pronamic_ideal' ); ?></h3>
+
+<?php if ( null === $pronamic_subscription ) : ?>
+
+	<?php esc_html_e( 'No Pronamic Pay subscription found for this WooCommerce subscription.', 'pronamic_ideal' ); ?>
+
+<?php else : ?>
+
+	<table>
+		<tbody>
+			<tr>
+				<th scope="row">
+					<?php esc_html_e( 'ID', 'pronamic_ideal' ); ?>
+				</th>
+				<td>
+					<?php edit_post_link( $pronamic_subscription->get_id(), '', '', $pronamic_subscription->get_id() ); ?>
+				</td>
+			</tr>
+			<tr>
+				<th scope="row">
+					<?php esc_html_e( 'Date', 'pronamic_ideal' ); ?>
+				</th>
+				<td>
+					<?php echo esc_html( $pronamic_subscription->date->format_i18n() ); ?>
+				</td>
+			</tr>
+		</tbody>
+	</table>
+
+<?php endif; ?>
+
+<hr>
+
+<h3><?php esc_html_e( 'Payment', 'pronamic_ideal' ); ?></h3>
+
+<?php if ( null === $pronamic_payment ) : ?>
 
 	<?php esc_html_e( 'No Pronamic Pay payment found for this WooCommerce subscription.', 'pronamic_ideal' ); ?>
 
@@ -36,7 +84,7 @@ $payment = get_pronamic_payment( $payment_id );
 					<?php esc_html_e( 'ID', 'pronamic_ideal' ); ?>
 				</th>
 				<td>
-					<?php edit_post_link( $payment->get_id(), '', '', $payment->get_id() ); ?>
+					<?php edit_post_link( $pronamic_payment->get_id(), '', '', $pronamic_payment->get_id() ); ?>
 				</td>
 			</tr>
 			<tr>
@@ -44,7 +92,7 @@ $payment = get_pronamic_payment( $payment_id );
 					<?php esc_html_e( 'Date', 'pronamic_ideal' ); ?>
 				</th>
 				<td>
-					<?php echo esc_html( $payment->date->format_i18n() ); ?>
+					<?php echo esc_html( $pronamic_payment->date->format_i18n() ); ?>
 				</td>
 			</tr>
 			<tr>
@@ -52,7 +100,15 @@ $payment = get_pronamic_payment( $payment_id );
 					<?php esc_html_e( 'Amount', 'pronamic_ideal' ); ?>
 				</th>
 				<td>
-					<?php echo esc_html( $payment->get_total_amount()->format_i18n() ); ?>
+					<?php echo esc_html( $pronamic_payment->get_total_amount()->format_i18n() ); ?>
+				</td>
+			</tr>
+			<tr>
+				<th scope="row">
+					<?php esc_html_e( 'Status', 'pronamic_ideal' ); ?>
+				</th>
+				<td>
+					<?php echo esc_html( $pronamic_payment->get_status_label() ); ?>
 				</td>
 			</tr>
 			<tr>
@@ -62,9 +118,9 @@ $payment = get_pronamic_payment( $payment_id );
 				<td>
 					<?php
 
-					$transaction_id = (string) $payment->get_transaction_id();
+					$transaction_id = (string) $pronamic_payment->get_transaction_id();
 
-					$url = (string) $payment->get_provider_link();
+					$url = (string) $pronamic_payment->get_provider_link();
 
 					if ( '' === $url ) {
 						echo esc_html( $transaction_id );
