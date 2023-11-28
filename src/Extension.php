d45053b75e85d99cd23a280f2e0d95e09e49155f
Remco Tolsma <869674+remcotolsma@users.noreply.github.com>
2023-11-28 11:42:30 +0100

    phpcbf

--- src/Extension.php
+++ src/Extension.php
@@ -294,10 +294,10 @@ class Extension extends AbstractPluginIntegration {
 				'icon'           => PaymentMethods::get_icon_url( PaymentMethods::BUNQ, $icon_size ),
 			],
 			[
-				'id'             => 'pronamic_pay_credit_card',
-				'payment_method' => PaymentMethods::CREDIT_CARD,
-				'icon'           => PaymentMethods::get_icon_url( PaymentMethods::CREDIT_CARD, $icon_size ),
-				'check_active'   => false,
+				'id'                 => 'pronamic_pay_credit_card',
+				'payment_method'     => PaymentMethods::CREDIT_CARD,
+				'icon'               => PaymentMethods::get_icon_url( PaymentMethods::CREDIT_CARD, $icon_size ),
+				'check_active'       => false,
 				'method_description' => \__(
 					'The most popular payment method in the world. Offers customers a safe and trusted way to pay online. Customers can pay for their order quickly and easily with their credit card, without having to worry about their security. It is possible to charge a payment surcharge for credit card costs.',
 					'pronamic_ideal'
